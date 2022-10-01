0834c1e5543c99bcf69777c8bb17c71e38db0918
kjkj70@naver.com <kjkj70@naver.com>
2022-10-02 00:02:23 +0900

    commit

--- app/src/main/java/com/example/swip/friends_manage.java
+++ app/src/main/java/com/example/swip/friends_manage.java
@@ -51,15 +51,9 @@ public class friends_manage extends AppCompatActivity {
             }
         });
 
-        Button friends_apply_btn = (Button) findViewById(R.id.friends_apply_btn);
-        friends_apply_btn.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                Intent intent = new Intent(getApplicationContext(), friends_apply.class);
-                startActivity(intent);
-            }
-        });
+        /* 하단바 */
 
+        /* 하단바 - 홈 */
         ImageButton nav_home = (ImageButton) findViewById(R.id.nav_home);
         nav_home.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -69,11 +63,45 @@ public class friends_manage extends AppCompatActivity {
             }
         });
 
+
+        /* 하단바 - 게시판 */
         ImageButton nav_post = (ImageButton) findViewById(R.id.nav_post);
         nav_post.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 Intent intent = new Intent(getApplicationContext(),information_board.class);
+                startActivity(intent);
+            }
+        });
+
+        /* 하단바 - 메뉴 */
+        ImageButton nav_menu = (ImageButton) findViewById(R.id.nav_menu);
+        nav_menu.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(getApplicationContext(), MenuActivity.class);
+                startActivity(intent);
+            }
+        });
+
+
+        /* 하단바 - 랭킹 */
+        ImageButton nav_rank = (ImageButton) findViewById(R.id.nav_rank);
+        nav_rank.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(getApplicationContext(), RankActivity.class);
+                startActivity(intent);
+            }
+        });
+
+
+        /* 하단바 - 알림 */
+        ImageButton btn_arlarm = (ImageButton) findViewById(R.id.nav_alarm);
+        btn_arlarm.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(getApplicationContext(), NofiActivity.class);
                 startActivity(intent);
             }
         });
