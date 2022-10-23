88449951b97f670e446e95f93e910a6addb44ed7
kjkj70@naver.com <kjkj70@naver.com>
2022-10-24 01:18:39 +0900

    commit

--- app/src/main/java/com/example/swip/TimerActivity.java
+++ app/src/main/java/com/example/swip/TimerActivity.java
@@ -2,17 +2,44 @@ package com.example.swip;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.os.SystemClock;
 import android.view.View;
+import android.widget.Button;
+import android.widget.Chronometer;
 import android.widget.ImageButton;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 public class TimerActivity extends AppCompatActivity {
 
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.timer);
+
+
+        final Chronometer chronometer = (Chronometer) findViewById(R.id.chronometer);
+        ImageButton buttonStart = (ImageButton) findViewById(R.id.buttonstart);
+        ImageButton buttonStop = (ImageButton) findViewById(R.id.buttonstop);
+
+        buttonStart.setOnClickListener(new Button.OnClickListener() {
+            public void onClick(View v) {
+                chronometer.start();
+                buttonStart.setVisibility(View.GONE);
+                buttonStop.setVisibility(View.VISIBLE);
+            }
+        });
+
+        buttonStop.setOnClickListener(new Button.OnClickListener() {
+            public void onClick(View v) {
+                chronometer.stop();
+                buttonStart.setVisibility(View.VISIBLE);
+                buttonStop.setVisibility(View.GONE);
+            }
+        });
+
+
 
         /* 하단바 */
 
