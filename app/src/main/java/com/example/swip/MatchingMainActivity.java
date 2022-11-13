b54e2b042f247af58b1c3b603309979f8938cf44
oblsoun <ychyung0615@gmail.com>
2022-11-13 17:46:19 +0900

    수정 필요

--- app/src/main/java/com/example/swip/MatchingMainActivity.java
+++ app/src/main/java/com/example/swip/MatchingMainActivity.java
@@ -1,25 +1,46 @@
 package com.example.swip;
 
+import android.content.ContentValues;
 import android.content.Intent;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.media.Image;
+import android.net.Uri;
+import android.net.UrlQuerySanitizer;
 import android.os.Bundle;
 import android.provider.ContactsContract;
+import android.provider.MediaStore;
 import android.view.View;
 import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ImageButton;
+import android.widget.ImageView;
 import android.widget.ListView;
+import android.widget.Toast;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 
+import com.bumptech.glide.Glide;
+import com.google.android.gms.common.internal.service.Common;
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.database.ChildEventListener;
 import com.google.firebase.database.DataSnapshot;
 import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.storage.FirebaseStorage;
+import com.google.firebase.storage.StorageReference;
+import com.google.firebase.storage.UploadTask;
 
+import java.io.IOException;
+import java.io.InputStream;
+import java.net.URL;
 import java.sql.Array;
 import java.util.ArrayList;
 import java.util.HashMap;
@@ -31,6 +52,8 @@ public class MatchingMainActivity  extends AppCompatActivity {
     private ImageButton btn_send;
     private EditText et_send;
     private ListView lv_chating;
+    private ImageButton btn_camera;
+    private ImageView st_img;
 
     private ArrayAdapter<String> arrayAdapter;
     private ArrayList<String> arr_room = new ArrayList<>();
@@ -42,6 +65,14 @@ public class MatchingMainActivity  extends AppCompatActivity {
     private String key;
     private String chat_user;
     private String chat_message;
+    private Object chat_photo;
+
+    private Uri fileUri;
+    private StorageReference storageReference;
+    private final int GALLERY_CODE = 10;
+    private FirebaseStorage storage;
+    ImageView load;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -51,6 +82,8 @@ public class MatchingMainActivity  extends AppCompatActivity {
         lv_chating = (ListView) findViewById(R.id.lv_chating);
         btn_send = (ImageButton) findViewById(R.id.btn_send);
         et_send = (EditText) findViewById(R.id.et_send);
+        btn_camera = (ImageButton) findViewById(R.id.btn_camera);
+        st_img = (ImageView) findViewById(R.id.st_img);
 
         str_room_name = getIntent().getExtras().get("room_name").toString();
         str_user_name = getIntent().getExtras().get("user_name").toString();
@@ -62,6 +95,51 @@ public class MatchingMainActivity  extends AppCompatActivity {
         lv_chating.setAdapter(arrayAdapter);
         // 리스트뷰가 갱신될 때 하단으로 자동 스크롤
         lv_chating.setTranscriptMode(ListView.TRANSCRIPT_MODE_ALWAYS_SCROLL);
+
+        btn_camera.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                switch (view.getId()) {
+                    case R.id.btn_camera:
+                        loadAlbum();
+                        break;
+                }
+                // map을 사용해 name과 메시지를 가져오고, key에 값 요청
+                Map<String, Object> map = new HashMap<String, Object>();
+                key = reference.push().getKey();
+                reference.updateChildren(map);
+
+                DatabaseReference root = reference.child(key);
+
+                load=(ImageView)findViewById(R.id.st_img);
+                FirebaseStorage storage = FirebaseStorage.getInstance();
+                StorageReference storageReference = storage.getReference();
+                StorageReference pathReference = storageReference.child("photo");
+                if (pathReference == null) {
+                    Toast.makeText(MatchingMainActivity.this, "저장소에 사진이 없습니다." ,Toast.LENGTH_SHORT).show();
+                } else {
+                    StorageReference submitImage = storageReference.child("photo/1.png");
+                    submitImage.getDownloadUrl().addOnSuccessListener(new OnSuccessListener<Uri>() {
+                        @Override
+                        public void onSuccess(Uri uri) {
+                            Glide.with(MatchingMainActivity.this).load(uri).into(st_img);
+
+                        }
+                    }).addOnFailureListener(new OnFailureListener() {
+                        @Override
+                        public void onFailure(@NonNull Exception e) {
+
+                        }
+                    });
+                }
+            }
+
+            private void loadAlbum() {
+                Intent intent = new Intent(Intent.ACTION_PICK);
+                intent.setType(MediaStore.Images.Media.CONTENT_TYPE);
+                startActivityForResult(intent, GALLERY_CODE);
+            }
+        });
 
         btn_send.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -111,6 +189,37 @@ public class MatchingMainActivity  extends AppCompatActivity {
             }
         });
     }
+    @Override
+    protected void onActivityResult(int requestCode, final int resultCode, final Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if (requestCode == GALLERY_CODE) {
+            Uri file = data.getData();
+            StorageReference storageRef = storage.getReference();
+            StorageReference riversRef = storageRef.child("photo/1.png");
+            UploadTask uploadTask = riversRef.putFile(file);
+
+            try {
+                InputStream in = getContentResolver().openInputStream(data.getData());
+                Bitmap img = BitmapFactory.decodeStream(in);
+                in.close();
+                st_img.setImageBitmap(img);
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+
+            uploadTask.addOnFailureListener(new OnFailureListener() {
+                @Override
+                public void onFailure(@NonNull Exception exception) {
+                    Toast.makeText(MatchingMainActivity.this, "사진이 정상적으로 업로드 되지 않았습니다.", Toast.LENGTH_SHORT).show();
+                }
+            }).addOnSuccessListener(new OnSuccessListener<UploadTask.TaskSnapshot>() {
+                @Override
+                public void onSuccess(UploadTask.TaskSnapshot taskSnapshot) {
+                    Toast.makeText(MatchingMainActivity.this, "사진이 정상적으로 업로드 되었습니다.", Toast.LENGTH_SHORT).show();
+                }
+            });
+        }
+    }
 
     // addChildEventListener를 통해 실제 데이터 베이스에 변경된 값이 있으면 화면에 보여지고 있는 ListView의 값을 갱신함
     private  void chatConversation(DataSnapshot snapshot) {
@@ -119,12 +228,12 @@ public class MatchingMainActivity  extends AppCompatActivity {
         while (i.hasNext()) {
             chat_message = (String) ((DataSnapshot) i.next()).getValue();
             chat_user = (String) ((DataSnapshot) i.next()).getValue();
+            chat_photo = (Image) ((DataSnapshot) i.next()).getValue();
 
             arrayAdapter.add(chat_user + " : " + chat_message);
+            arrayAdapter.add(chat_user + " : " + chat_photo);
         }
 
         arrayAdapter.notifyDataSetChanged();
     }
-
-
 }
